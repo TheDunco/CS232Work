82c09ed1d505ff050eeb6a5ca7b3bf64a5f90346
TheDunco <duncvan@gmail.com>
2021-04-26 15:23:34 -0400

    changed print to println and it worked

--- Homework7/CaesarCipherClient.java
+++ Homework7/CaesarCipherClient.java
@@ -1,21 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/*
+Based on these two examples...
+https://www.infoworld.com/article/2853780/socket-programming-for-scalable-systems.html
+https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+*/
+
 public class CaesarCipherClient {
+
+    Socket socket;
+    PrintStream socketWriter;
+    BufferedReader socketReader;
+    Scanner userInput;
+
     public static void main (String[] args) {
         if (args.length == 2) {
             CaesarCipherClient me = new CaesarCipherClient(args[0], Integer.parseInt(args[1]));
+            me.run();
         }
         else {
             System.out.println("Usage: java CaesarCipherClient <host> <port>");
             System.exit(0);
         }
-
     }
 
     public CaesarCipherClient(String host, int port) {
@@ -23,60 +34,73 @@ public class CaesarCipherClient {
 
             System.out.println("Host: " + host + "\nPort: " + port);
             // Initialize socket
-            Socket socket = new Socket(host, port);
+            this.socket = new Socket(host, port);
 
             // Initialize in and out to read from and write to the socket
-            PrintStream out = new PrintStream( socket.getOutputStream() );
-            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ));
+            this.socketWriter = new PrintStream( socket.getOutputStream() );
+            this.socketReader = new BufferedReader( new InputStreamReader( socket.getInputStream() ));
 
             // Initialize a scanner to grab input from the user
-            Scanner input = new Scanner(System.in);
+            this.userInput = new Scanner(System.in);
+        } 
+        catch( Exception e ) {
+            e.printStackTrace();
+        }
+    }
 
+    public void run() {
+        try {
             System.out.print("Welcome to the CaesarCipherClient! \nEnter your rotation number: ");
-
+    
             int rotationNumber = 0;
-
+    
             // Grab the rotation number from the user
             try {
-                rotationNumber = input.nextInt();
-
+                rotationNumber = this.userInput.nextInt();
+    
             }
             catch (InputMismatchException ime) {
                 System.out.println("Invalid input: please enter an integer");
-
-                input.close();
-                in.close();
-                out.close();
-                socket.close();
-
+    
+                this.closeAll();
+    
                 throw ime;
             }
-
+    
             System.out.println("Thanks! Rotation number entered: " + rotationNumber);
-
+    
             // Attempt to contact the server with that rotation number
-            out.print(rotationNumber);
+            socketWriter.println(rotationNumber);
             System.out.println("Attempting to contact server...");
-
+    
             // Wait for the server's response and respond appropriately
             int response = 0;
             try { 
-                response = Integer.parseInt(in.readLine());
+                response = Integer.parseInt(socketReader.readLine());
             }
             catch(java.io.IOException e) {
                 System.out.println(e);
             }
-
+    
             System.out.println("Got response: " + response);
+    
+            this.closeAll();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            // Close everything that's open to avoid leaks
-            input.close();
-            in.close();
-            out.close();
-            socket.close();
-
-        } 
-        catch( Exception e ) {
+    // Close all open sockets/readers/writers to avoid leaks
+    private void closeAll() {
+        try {
+            this.userInput.close();
+            this.socketReader.close();
+            this.socketWriter.close();
+            this.socket.close();
+        }
+        catch (Exception e) {
+            System.out.println("Closing failed");
             e.printStackTrace();
         }
     }
